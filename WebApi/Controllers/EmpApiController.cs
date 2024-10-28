aa5a128dce209de43e7c63fd6244e0ea59132c4d
deval <deval@SAUMYA>
2024-10-28 20:47:43 +0530

    Add project files.

---
+++ WebApi/Controllers/EmpApiController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmpApiController : ApiController
+    {
+        MVCWebDBEntities1 ent = new MVCWebDBEntities1();
+
+        [HttpGet]
+        public IHttpActionResult GetEmployees()
+        {
+            List<Employee> list = ent.Employees.ToList();
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetEmployeesById(int id)
+        {
+            var emp = ent.Employees.Where(m=>m.Eid == id).FirstOrDefault();
+            return Ok(emp);
+        }
+
+        [HttpPost]
+        public IHttpActionResult EmplInsert(Employee emp)
+        {
+            ent.Employees.Add(emp);
+            ent.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult EmpUpdate(Employee e)
+        {
+            ent.Entry(e).State = System.Data.Entity.EntityState.Modified;
+            ent.SaveChanges();
+
+
+            //var emp = ent.Employees.Where(m=>m.Eid==e.Eid).FirstOrDefault();
+            //if (emp != null) 
+            //{
+            //    emp.Eid = e.Eid;
+            //    emp.EmpName = e.EmpName;
+            //    emp.DeptID = e.DeptID;
+            //    ent.SaveChanges();
+            //}
+            //else
+            //{
+            //    return NotFound();
+            //}
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult EmpDelete(int id)
+        {
+            var emp = ent.Employees.Where(m => m.Eid == id).FirstOrDefault();
+            ent.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+            ent.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ WebApi/Controllers/MvcController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApi.Models;
+
+
+namespace WebApi.Controllers
+{
+    public class MvcController : Controller
+    {
+        // GET: Mvc
+
+        HttpClient client = new HttpClient();  
+        public ActionResult Index()
+        {
+            List<Employee> emplist = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44342/api/EmpApi");
+            var response = client.GetAsync("EmpApi");
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                emplist = display.Result;
+            }
+            return View(emplist);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            client.BaseAddress = new Uri("https://localhost:44342/api/EmpApi");
+            var response = client.PostAsJsonAsync<Employee>("EmpApi",emp);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+             return View("Create");
+        }
+
+        public ActionResult Details(int id) 
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44342/api/EmpApi");
+            var response = client.GetAsync("EmpApi?id="+id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44342/api/EmpApi");
+            var response = client.GetAsync("EmpApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            client.BaseAddress = new Uri("https://localhost:44342/api/EmpApi");
+            var response = client.PutAsJsonAsync<Employee>("EmpApi", e);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44342/api/EmpApi");
+            var response = client.GetAsync("EmpApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+
+        [System.Web.Mvc.HttpPost,System.Web.Mvc.ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44342/api/EmpApi");
+            var response = client.DeleteAsync("EmpApi/"+id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+        }
+    }
+}
